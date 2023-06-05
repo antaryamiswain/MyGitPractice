68fb45f0c925e115cc16c2819be4010b30abf639
antaryamiswain <antaryami.swain289@gmail.com>
2023-06-05 21:21:16 +0530

    Update myfirstJava.java

--- myfirstJava.java
+++ myfirstJava.java
@@ -4,6 +4,7 @@ public static void main(String argsp[])
 {
   System.out.println("Hello...Welcome to Git");
 System.out.println("new changes for push1");
+  System.out.println("new changes for pull done the changes in git hub");
 }
 
 }
